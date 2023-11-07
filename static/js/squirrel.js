4fe5250ebbd3ccf6af177fdab2025d72eb474831
Alyssa H <alyssa_hondrade@icloud.com>
2023-11-07 21:40:20 +0800

    added skeleton function to javascript

--- static/js/squirrel.js
+++ static/js/squirrel.js
@@ -48,15 +48,33 @@ console.log("Testing HTML");
 const locations_url = "http://127.0.0.1:5000/locations";
 const appearance_url = "http://127.0.0.1:5000/appearance";
 const activities_url = "http://127.0.0.1:5000/activities";
-const interactions_url = "http://127.0.0.1:5000/activities";
+const interactions_url = "http://127.0.0.1:5000/interactions";
+
+
+function create_plots(location_data, appearance_data, activities_data, interactions_data) {
+    console.log("I'm in the function");
+    console.log(location_data);
+    console.log(appearance_data);
+    console.log(activities_data);
+    console.log(interactions_data);
+};
+
+
+
+
+
+
 
 // const url = "localhost:8000/locations";
 console.log("HERE??");
 
 d3.json(locations_url).then(function(location_data) {
-    console.log(location_data);
     d3.json(appearance_url).then(function(appearance_data) {
-        console.log(appearance_data);
+        d3.json(activities_url).then(function(activities_data) {
+            d3.json(interactions_url).then(function(interactions_data) {
+                create_plots(location_data, appearance_data, activities_data, interactions_data);
+            });
+        });
     });
 });
 
