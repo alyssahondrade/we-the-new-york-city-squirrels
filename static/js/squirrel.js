df1c81670efc3898876242d08dfa681d63cfc0f2
Alyssa H <alyssa_hondrade@icloud.com>
2023-11-08 08:57:44 +0800

    created basic map will all squirrel sighting locations

--- static/js/squirrel.js
+++ static/js/squirrel.js
@@ -45,11 +45,15 @@
 
 console.log("Testing HTML");
 
+// Define the URLs for the dataset
 const locations_url = "http://127.0.0.1:5000/locations";
 const appearance_url = "http://127.0.0.1:5000/appearance";
 const activities_url = "http://127.0.0.1:5000/activities";
 const interactions_url = "http://127.0.0.1:5000/interactions";
 
+// Define the map parameters
+let map_centre = [40.730610, -73.935242]; // New York City. https://www.latlong.net/place/new-york-city-ny-usa-1848.html
+let map_zoom = 11;
 
 function create_plots(location_data, appearance_data, activities_data, interactions_data) {
     console.log("I'm in the function");
@@ -59,7 +63,59 @@ function create_plots(location_data, appearance_data, activities_data, interacti
     console.log(interactions_data);
 };
 
+function create_map(layer) {
+    // Create the street tile layer
+    let street_tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy;\
+            <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>\
+            contributors &copy;\
+            <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
+    });
 
+    // Create the base maps object
+    let base_maps = {
+        Street: street_tiles
+    };
+
+    // Create overlay maps object
+    let overlay_maps = {
+        TestLayer: layer
+    };
+
+    // Create the map
+    let my_map = L.map("map", {
+        center: map_centre,
+        zoom: map_zoom,
+        layers: [street_tiles],
+        worldCopyJump: true
+    });
+
+    // Create the layer control and add to the map
+    L.control.layers(base_maps, overlay_maps).addTo(my_map);
+    
+};
+
+function create_map_markers(location_data) {
+    // console.log(location_data)
+
+    let location_markers = [];
+    for (let i=0; i<location_data.length; i++) {
+        console.log(location_data[i]);
+        let latitude = location_data[i].latitude
+        let longitude = location_data[i].longitude
+
+        let marker = L.circleMarker([latitude, longitude], {
+            radius: 10
+        });
+
+        location_markers.push(marker);
+    };
+
+    let markers_layer = L.layerGroup(location_markers);
+    create_map(markers_layer);
+};
 
 
 
@@ -72,7 +128,8 @@ d3.json(locations_url).then(function(location_data) {
     d3.json(appearance_url).then(function(appearance_data) {
         d3.json(activities_url).then(function(activities_data) {
             d3.json(interactions_url).then(function(interactions_data) {
-                create_plots(location_data, appearance_data, activities_data, interactions_data);
+                // create_plots(location_data, appearance_data, activities_data, interactions_data);
+                create_map_markers(location_data);
             });
         });
     });
