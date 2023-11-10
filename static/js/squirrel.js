732f2380c326d0be3b65b07e708b0a41a9f2a4e7
Alyssa H <alyssa_hondrade@icloud.com>
2023-11-10 21:49:44 +0800

    metadata table has a default value and updates correctly

--- static/js/squirrel.js
+++ static/js/squirrel.js
@@ -439,7 +439,35 @@ function sighting_metadata(dataset, metadata, squirrel_id, appearance_data) {
         // Get the squirrel specifics
         let squirrel_metadata = metadata.filter(find_id)[0];
         let squirrel_appearance = appearance_data.filter(find_id)[0];
+        
         console.log(squirrel_metadata, squirrel_appearance);
+
+        // Get the squirrel highlights
+        let highlight_options = remove_keys(Object.keys(squirrel_appearance), 'squirrel_id', 'primary_colour');
+
+        let squirrel_highlights = [];
+        for (let i=0; i<highlight_options.length; i++) {
+            if (squirrel_appearance[highlight_options[i]]) {
+                squirrel_highlights.push(highlight_options[i])
+            };
+        };
+
+        // Format the highlights list
+        let formatted_highlights = squirrel_highlights.map(word => word[0].toUpperCase() + word.substring(1));
+
+        // Update the table
+        let info_id = d3.select("#meta_id").text(squirrel_id);
+        
+        let info_date = d3.select("#meta_date").text(
+            `${squirrel_metadata.day}-${squirrel_metadata.month}-${squirrel_metadata.year}`);
+        
+        let info_coords = d3.select("#meta_coords").text(
+            `[${squirrel_metadata.latitude.toFixed(6)}, ${squirrel_metadata.longitude.toFixed(6)}]`);
+        
+        let info_primary = d3.select("#meta_primary").text(`${squirrel_appearance.primary_colour}`);
+
+        let info_highlights = d3.select("#meta_highlights").text(formatted_highlights.join(", "));
+
     };
     
 };
