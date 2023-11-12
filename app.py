f57e29588b4452f6bb364f5192649c8cceae6288
Alyssa H <alyssa_hondrade@icloud.com>
2023-11-12 09:12:51 +0800

    app.py cleanup

--- app.py
+++ app.py
@@ -27,64 +27,49 @@ session = Session(bind=engine)
 inspector = inspect(engine)
 table_names = inspector.get_table_names()
 
+# Initialise the dictionary that will hold the results
 endpoint_data = dict()
+
+# Loop through each table in the database
 for table in table_names:
+    
     # Get the column names for the table
     table_columns = inspector.get_columns(table)
 
+    # Extract the name from each column
     col_names = []
     for col in table_columns:
         col_names.append(col['name'])
 
+    # SQL query to return everything from a table
     statement = f"SELECT * FROM {table}"
-    with engine.connect() as conn:        
-        results = conn.execute(text(statement))
-        # results = conn.execute(text(f"SELECT * FROM {table}"))
-        # results = engine.execute(f"SELECT * FROM {table}").fetchall()
 
+    # Connect to the database
+    with engine.connect() as conn:
+        
+        # Execute the statement
+        results = conn.execute(text(statement))
+
+        # Loop through each row in the table
         output_list = []
         for result in results:
             output_dict = dict()
-    
+
+            # Loop through each value in the row
             for col in col_names:
                 output_dict[col] = getattr(result, col)
             output_list.append(output_dict)
-    
+
+        # Populate the dictionary
         endpoint_data[table] = output_list
 
-# print(endpoint_data['appearance'])
-
-
-# # Get column names for 'locations'
-# locations_cols = inspector.get_columns('locations')
-
-# col_names = []
-# for col in locations_cols:
-#     col_names.append(col['name'])
-
-# # Get the dictionary for 'locations'
-# results = engine.execute("SELECT * FROM locations").fetchall()
-
-# output_list = []
-# for result in results:
-#     # result_dict = result.__dict__ # This only works in jupyter notebook?
-#     output_dict = dict()
-
-#     # for col in col_names:
-#     #     output_dict[col] = result_dict.get(col)
-#     # output_list.append(output_dict)
-    
-#     # print(dir(result))
-#     for col in col_names:
-#         # print(getattr(result, col))
-#         output_dict[col] = getattr(result, col)
-#     output_list.append(output_dict)
 
 #################################################
 # Flask Setup
-app = Flask(__name__)
-CORS(app)
 #################################################
+app = Flask(__name__)
+
+CORS(app)
 
 #################################################
 # Flask Routes
@@ -99,44 +84,24 @@ def homepage():
         f"/activities<br>"
         f"/interactions<br>"
     )
+    # return render_template("index.html")
 
 @app.route("/metadata")
-# @app.route("/locations", methods=['GET'])
 def locations_route():
     return jsonify(endpoint_data['metadata'])
-    # response = jsonify(endpoint_data['locations'])
-    # response.headers.add('Access-Control-Allow-Origin', '*')
-    # return response
 
 @app.route("/appearance")
-# @app.route("/appearance", methods=['GET'])
 def appearance_route():
     return jsonify(endpoint_data['appearance'])
-    # response = jsonify(endpoint_data['appearance'])
-    # response.headers.add('Access-Control-Allow-Origin', '*')
-    # return response
 
 @app.route("/activities")
-# @app.route("/activities", methods=['GET'])
 def activities_route():
     return jsonify(endpoint_data['activities'])
-    # response = jsonify(endpoint_data['activities'])
-    # response.headers.add('Access-Control-Allow-Origin', '*')
-    # return response
 
 @app.route("/interactions")
-# @app.route("/interactions", methods=['GET'])
 def interactions_route():
     return jsonify(endpoint_data['interactions'])
-    # response = jsonify(endpoint_data['interactions'])
-    # response.headers.add('Access-Control-Allow-Origin', '*')
-    # return response
 
-# @app.route('your route', methods=['GET'])
-# def yourMethod(params):
-#     response = flask.jsonify({'some': 'data'})
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
 
 if __name__ == '__main__':
     app.run(debug=True)
